899fd5baecfce3d9eb01430e1ff93962be8ac6fa
Brescher <73077282+Brescher@users.noreply.github.com>
2020-11-21 16:32:30 +0100

    prechod na php
    refactoring suborov na php

---
+++ nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-sm navbar-dark">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/semestralnaPraca/index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/semestralnaPraca/news.php">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/semestralnaPraca/support.php">Support</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/semestralnaPraca/login.php">Log in</a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ support.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Support</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styl.css">
+</head>
+<?php include "nav.php" ?>
+<body>
+
+<div class="container h-100">
+    <div class="row align-items-center h-100">
+        <div class="col-6 mx-auto">
+            <div class="jumbotron">
+                <img src="img/techDiff2.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
